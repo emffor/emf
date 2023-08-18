4948fd4e2b50f7c074aeea23792f4fca65314358
E Ferreira <98027423+emffor@users.noreply.github.com>
2023-08-17 21:22:06 -0300

    fix - excluir e alguns ajustes na aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Box, ChakraProvider, Grid, VStack, theme } from "@chakra-ui/react";
+import { Logo } from "./components/Logo";
+import { ColorModeSwitcher } from "./config/ColorModeSwitcher";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -19,20 +9,8 @@ export const App = () => (
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
